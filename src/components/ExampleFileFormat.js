b11154bb7eb0652a6879174a057b38464130e1d3
InzishKhan <inzishk56@gmail.com>
2026-03-14 15:18:28 +0500

    fix:error on display changed to new line

--- src/components/ExampleFileFormat.js
+++ src/components/ExampleFileFormat.js
@@ -6,13 +6,12 @@ function ExampleFileFormat({ compact = true }) {
       <h2 className="bm-section-title">Example bond file format</h2>
       <div className="bm-file-example">
         <div className="bm-file-example-title">bonds.txt</div>
-        <div>
+        <div style={{ whiteSpace: 'pre-line' }}>
           123456{'\n'}
           987654{'\n'}
           456789{'\n'}
           112233
-        </div>
-        <div className="bm-file-example-footer">
+        </div>        <div className="bm-file-example-footer">
           One bond number per line. No commas, spaces, or extra text.
         </div>
       </div>
